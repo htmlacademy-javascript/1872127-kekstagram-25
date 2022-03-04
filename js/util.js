a4e16eb5ad5ac49a4a16c313ee34feb6ed1141ca
Denis Makushin <the.makushin@yandex.ru>
2022-03-04 21:54:08 +0300

    Правки линтер

--- js/util.js
+++ js/util.js
@@ -1,20 +1,20 @@
 // Функция генерации случайного числа из диапазона
 function getInteger(minInt, maxInt) {
-    if (minInt >= 0) {
-      if (minInt < maxInt) {
-        minInt = Math.ceil(minInt);
-        maxInt = Math.floor(maxInt);
-        return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
-      }
-      return Math.floor(Math.random() * (minInt - maxInt + 1)) + maxInt; // Меняем местами
+  if (minInt >= 0) {
+    if (minInt < maxInt) {
+      minInt = Math.ceil(minInt);
+      maxInt = Math.floor(maxInt);
+      return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
     }
-    return 'Не может быть меньше нуля';
+    return Math.floor(Math.random() * (minInt - maxInt + 1)) + maxInt; // Меняем местами
   }
+  return 'Не может быть меньше нуля';
+}
 
- // Функция, возвращающая случайный элемент массива
- function getRandomArrayElement(elements) {
-    return elements[getInteger(0, elements.length - 1)];
-  }
+// Функция, возвращающая случайный элемент массива
+function getRandomArrayElement(elements) {
+  return elements[getInteger(0, elements.length - 1)];
+}
 
 export {getInteger};
 export {getRandomArrayElement};
