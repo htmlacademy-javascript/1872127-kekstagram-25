b4540c37d1f877c2a09340d753cf175f4730d44b
Keks <keks@htmlacademy.ru>
2022-03-27 20:43:40 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/fullscreen-photo.js
+++ 8/js/fullscreen-photo.js
@@ -1,24 +1,44 @@
+import {isEscapeKey} from './util.js';
 const bigPicture = document.querySelector('.big-picture');
 const commentsList = document.querySelector('.social__comments');
-const commentItem = document.querySelector('.social__comment');
-const commentTemplate = document.querySelector('#commentary').content;
+const commentTemplate = document.querySelector('#commentary').content.querySelector('.social__comment');
 
-const fullscreenPhoto = (photo) => {
-  bigPicture.classList.remove('hidden');
+const renderCommentAuthor = (comment) => {
+  const commentElement = commentTemplate.cloneNode(true);
+  commentElement.querySelector('.social__picture').src = comment.avatar;
+  commentElement.querySelector('.social__picture').alt = comment.name;
+  commentElement.querySelector('.social__text').textContent = comment.message;
+  return commentElement;
+};
+
+const onPopupEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeFullscreenPhoto();
+  }
+};
+
+function closeFullscreenPhoto () {
+  bigPicture.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscKeydown);
+}
+
+const openFullscreenPhoto = (photo) => {
   bigPicture.querySelector('.big-picture__img').src = photo.url;
   bigPicture.querySelector('.likes-count').textContent = photo.likes;
   bigPicture.querySelector('.comments-count').textContent = photo.comments.length;
   bigPicture.querySelector('.social__caption').textContent = photo.description;
-
-  const commentAuthor = (comment) => {
-    const commentElement = commentTemplate.cloneNode(true);
-    commentItem.appendChild(commentElement);
-    commentElement.querySelector('.social__picture').src = comment.avatar;
-    commentElement.querySelector('.social__picture').alt = comment.name;
-    commentElement.querySelector('.social__text').textContent = comment.message;
-  };
-
-  commentsList.appendChild(commentAuthor);
+  bigPicture.classList.remove('hidden');
+  commentsList.append(...photo.comments.map(renderCommentAuthor));
+  bigPicture.querySelector('.big-picture__cancel').addEventListener('click', () => {
+    closeFullscreenPhoto();
+  });
+  document.addEventListener('keydown', onPopupEscKeydown);
 };
 
-export {fullscreenPhoto};
+// const showMoreComments = () => {
+//   bigPicture.querySelector('.social__comment-count').classList.remove('hidden');
+//   bigPicture.querySelector('.comments-loader').classList.remove('hidden');
+// };
+
+export {openFullscreenPhoto};
--- 8/js/main.js
+++ 8/js/main.js
@@ -1,10 +1,8 @@
 import {createPhotos} from './data.js';
 import {renderPhotos} from './render.js';
-import {fullscreenPhoto} from './fullscreen-photo.js';
-import {modalPhotos} from './processing-modal.js';
+import {renderModalPhotos} from './processing-modal.js';
 import {initUploadWindow} from './validator.js';
 const photos = createPhotos();
 renderPhotos(photos);
-fullscreenPhoto();
-modalPhotos();
+renderModalPhotos();
 initUploadWindow();
