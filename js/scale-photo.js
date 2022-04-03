6317ba3fbbf1f8d6340f8100e2f6ed06b0a32c0a
Denis Makushin <the.makushin@yandex.ru>
2022-04-03 18:08:28 +0300

    Доработаны кнопки масштабирования без %

--- js/scale-photo.js
+++ js/scale-photo.js
@@ -3,36 +3,32 @@ const controlBigger = document.querySelector('.scale__control--bigger');
 const controlValue = document.querySelector('.scale__control--value');
 const imgUploadPreview = document.querySelector('.img-upload__preview img');
 
-const SCALE_MIN = 25;
-const SCALE_MAX = 100;
-const SCALE_STEP = 25;
+const SCALE_MIN = 0.25;
+const SCALE_MAX = 1;
+const SCALE_STEP = 0.25;
 const SCALE_DEFAULT = 100;
 
 const runDefaultScale = () => {
   controlValue.value = SCALE_DEFAULT;
-  imgUploadPreview.style.cssText = 'transform: scale(1)';
+  imgUploadPreview.style.transform = 'scale(1)';
 };
 
-const onButton = () => {
-  controlSmaller.addEventListener('click', () => {
-    const amount = controlValue.value - SCALE_STEP;
-    imgUploadPreview.style.cssText = `transform: scale(0.'${amount}')`;
-    controlValue.value = amount;
-  });
+controlSmaller.addEventListener('click', () => {
+  controlValue.value.replace('%', '');
+  if ((Number(controlValue.value) / 100) != SCALE_MIN) {
+    const amount = controlValue.value / 100 - SCALE_STEP;
+    imgUploadPreview.style.transform = `scale(${amount})`;
+    controlValue.value = amount * 100;
+  };
+});
 
-  controlBigger.addEventListener('click', () => {
-    const amount = controlValue.value + SCALE_STEP;
-    imgUploadPreview.style.cssText = `transform: scale(0.'${amount}')`;
-    controlValue.value = amount;
-  });
-};
+controlBigger.addEventListener('click', () => {
+  controlValue.value.replace('%', '');
+  if ((Number(controlValue.value) / 100) != SCALE_MAX) {
+    const amount = controlValue.value / 100 + SCALE_STEP;
+    imgUploadPreview.style.transform = `scale(${amount})`;
+    controlValue.value = amount * 100;
+  };
+});
 
-controlValue.onchange = () => {
-  if (controlValue.value <= SCALE_MAX && controlValue.value >= SCALE_MIN) {
-    onButton();
-  }
-  else {
-    runDefaultScale();
-  }
-};
-
+export {runDefaultScale};
