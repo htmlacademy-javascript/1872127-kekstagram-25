3c3c60b9ed828e4dd0878285355360dfd0858628
Denis Makushin <the.makushin@yandex.ru>
2022-04-11 23:36:33 +0300

    Повторные правки

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-import {renderPhotos} from './render.js';
+import {renderPhotos, setDiscussedClick, setRandomClick, setDefaultClick} from './render.js';
 import {renderModalPhotos} from './processing-modal.js';
 import {initUploadWindow} from './validator.js';
 import './slider-photo.js';
 import './scale-photo.js';
 import {getData} from './api.js';
+import {debounce} from './util.js';
 renderModalPhotos();
 initUploadWindow();
 
 getData((photos) => {
   renderPhotos(photos);
   document.querySelector('.img-filters').classList.remove('img-filters--inactive');
+  setDefaultClick(debounce(() => renderPhotos(photos)));
+  setDiscussedClick(debounce(() => renderPhotos(photos)));
+  setRandomClick(debounce(() => renderPhotos(photos)));
 },
 () => {},
 );
--- js/render.js
+++ js/render.js
@@ -14,72 +14,55 @@ const renderPhoto = (photo) => {
   return photoElement;
 };
 
-const PHOTOS_COUNT = 25;
-
-const getPhotosRank = (indication) => {
-  const urlPhotoRecorded = document.querySelector('.picture__img').src; // адрес изображения
-  const commentsPhotoRecorded = document.querySelector('.picture__comments').textContent; // количество комментариев
-
-  let rank = 0;
-// присвоение ранга исходя из количества комментариев
-  if ((indication.textContent === commentsPhotoRecorded) <= 5) {
-    rank += 1;
-  }
-
-  if ((indication.textContent === commentsPhotoRecorded) > 5 && (indication.textContent === commentsPhotoRecorded) <= 10) {
-    rank += 2;
-  }
-
-  if ((indication.textContent === commentsPhotoRecorded) > 10 && (indication.textContent === commentsPhotoRecorded) <= 15) {
-    rank += 3;
-  }
-
-  if ((indication.textContent === commentsPhotoRecorded) > 15) {
-    rank += 4;
-  }
-// если изображения не похожи
-  if (indication.url !== urlPhotoRecorded.value) { 
-    rank += 5;
-  }
-
-  return rank;
-};
-// и потом при сортировке по популярности построим массив на убывание
-// по случайным фотографиям укажем ранг не ниже 5
-
-const comparePhotos = (photoA, photoB) => {
-  const rankA = getPhotosRank(photoA);
-  const rankB = getPhotosRank(photoB);
-
-  return rankB - rankA;
-};
-// надо ли удалять clearPhotos() и вместо него писать photoBox.innerHTML =''? Какая цель? В чем разница?
 const clearPhotos = () => {
   photoBox.querySelectorAll('.picture').forEach((element) => element.remove());
 };
 
-const setDiscussedClick = (cb) => {
-  document.querySelector('#filter-discussed').addEventListener('click', (evt) => {
-
-  });
-};
-
-const setRandomClick = (cb) => {
-  document.querySelector('#filter-random').addEventListener('click', (evt) => {
-
-  });
-};
+const PHOTOS_COUNT = 25;
 
 const renderPhotos = (photos) => {
   clearPhotos();
   const photoFragment = document.createDocumentFragment();
   photos
-  .slice(0, PHOTOS_COUNT)
-  .sort(comparePhotos)
-  .forEach((item) => {
-    photoFragment.appendChild(renderPhoto(item));
-  });
+    .slice(0, PHOTOS_COUNT)
+    .sort(comparePhotos)
+    .forEach((item) => {
+      photoFragment.appendChild(renderPhoto(item));
+    });
   photoBox.appendChild(photoFragment);
 };
 
-export {renderPhotos, clearPhotos, setDiscussedClick, setRandomClick};
+function comparePhotos (photoA, photoB) {
+  return photoB.comments.length - photoA.comments.length;
+}
+// надо ли удалять clearPhotos() и вместо него писать photoBox.innerHTML =''? Какая цель? В чем разница?
+const setDefaultClick = (photos) => {
+  document.querySelector('#filter-discussed').addEventListener('click', (evt) => {
+    document.querySelector('#filter-random').classList.remove('img-filters__button--active');
+    document.querySelector('#filter-discussed').classList.remove('img-filters__button--active');
+    evt.target.classList.add('img-filters__button--active');
+    renderPhotos(photos);
+  });
+};
+
+const setDiscussedClick = (photos) => {
+  document.querySelector('#filter-discussed').addEventListener('click', (evt) => {
+    document.querySelector('#filter-default').classList.remove('img-filters__button--active');
+    document.querySelector('#filter-random').classList.remove('img-filters__button--active');
+    evt.target.classList.add('img-filters__button--active');
+    const mostCommented = photos.forEach(comparePhotos);
+    renderPhotos(mostCommented);
+  });
+};
+
+const setRandomClick = (photos) => {
+  document.querySelector('#filter-random').addEventListener('click', (evt) => {
+    document.querySelector('#filter-discussed').classList.remove('img-filters__button--active');
+    document.querySelector('#filter-random').classList.remove('img-filters__button--active');
+    evt.target.classList.add('img-filters__button--active');
+    const showRandomly = photos.sort(comparePhotos).slice(10);
+    renderPhotos(showRandomly);
+  });
+};
+
+export {renderPhotos, clearPhotos, setDiscussedClick, setRandomClick, setDefaultClick};
